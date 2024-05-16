f92a5082d3379330bd45196bc91b0a4ec12dba59
Leifeng2000 <leifeng7112@gmail.com>
2024-05-16 22:20:14 +0700

    Update

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public float fuel = 0;
+    public int roundNumer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetFuel(float fuel)
+    {
+        this.fuel += fuel;
+    }
+    public void SetRound(int roundNumber)
+    {
+        this.roundNumer += roundNumber;
+    }
+}
